2ee108ca8ec07023933514c02282661102c3554b
aliceheneghan <heneghan31@gmail.com>
2022-08-16 12:48:34 +0200

    installed React Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+
+// Styles
 import './index.css';
+
+// Components
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );

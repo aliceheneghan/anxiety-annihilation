95fdec0f6833166122d77164919ca54e1bf40b71
aliceheneghan <heneghan31@gmail.com>
2022-08-17 20:42:20 +0200

    called BorderBubble inside BeginButton instead of vice versa

--- src/components/BeginButton/index.jsx
+++ src/components/BeginButton/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
+// Components
+import BorderBubble from "../../components/BorderBubble";
+
+
 export default function BeginButton({ handleNav }) {
   return (
-    <div className="title-container flx cnt-cnt">
+    <div className="title-container flx column cnt-cnt">
       <button
         className="title-font xxl text-center title"
         onClick={() => handleNav("/choice")}
       >
         Anxiety Annihilation
       </button>
+      <BorderBubble  />
     </div>
   );
 }

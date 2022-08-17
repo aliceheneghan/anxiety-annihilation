cf1947051bb57f8665a9503e1e28f9b5f4e5bbe4
aliceheneghan <heneghan31@gmail.com>
2022-08-17 20:16:24 +0200

    wrapped StyledLink in a button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Header() {
-    
+  const StyledLink  = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
   return (
-    <div className="header-cont">
-        <nav>
-            <NavLink to="/" style={({ isActive }) => { 
-                return isActive ? { backgroundColor: "pink" } : undefined;
-             }}>Home</NavLink>
-            <NavLink to="/Choice">Choice</NavLink>
-            <NavLink to="/ListenAndLook">ListenAndLook</NavLink>
+    <div className="nav-cont flx cnt-cnt">
+        <nav className="flx spc-even ">
+            <button><StyledLink to="/">Home</StyledLink></button>
+            <button><StyledLink to="/Choice">Choice</StyledLink></button>
+            <button><StyledLink to="/ListenAndLook">ListenAndLook</StyledLink></button>
         </nav>
     {/* <button onClick={handleNavigate}>Practise navigate</ button> */}
     </div>

f645b7f9f5fa91ceb3b738c1b6993f1aa81cac2a
aliceheneghan <heneghan31@gmail.com>
2022-08-17 16:17:02 +0200

    divided jsx igit pushnto header and section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
-import Choice from "./pages/Choice";
-import ListenAndLook from "./pages/ListenAndLook";
-import NotFound from "./pages/NotFound";
+import ChoiceRouter from "./pages/ChoiceRouter";
 
 // Components
 import Header from "./components/Header";
 
 function App() {
+  // navigate
+  const navigate = useNavigate()
+
+  // handlers
+  const handleNav = (path) => {
+        navigate(`${path}`)
+    }; 
+
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/Choice" element={<Choice />}/>
-        <Route path="/ListenAndLook" element={<ListenAndLook />}/>
-        {/* Fallback route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+    <div className="App flx strt-cnt column">
+      <header >
+        <Header />
+      </header>
+      <section className="flx cnt-cnt">
+        <Routes >
+          <Route path="/" element={<Home handleNav={handleNav} />} />
+          <Route path="/choice/*" element={<ChoiceRouter />} />
+
+          {/* Fallback route 
+            <Route
+              path="*"
+              element={<NotFound />}
+            /> /* (import Navigate component from react router dom) <Navigate to={"/"} /> */
+          }
+        </Routes>
+      </section>
     </div>
   );
 }

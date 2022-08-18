62fb6fed73db5398933f70da371ad3958a02a09e
aliceheneghan <heneghan31@gmail.com>
2022-08-18 12:25:06 +0200

    created randomNumber state and used to select random image from imageArray, called ContentButton and passed handleClick

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,20 +1,33 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-export default function Image( { url }) {
-    const fetchRandomImage = async () => {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    }
-    // states
-    const [image, setImage] = useState({})
-    // effects
-    useEffect(() => {
-        fetchRandomImage().then((data => setImage(data.message)))
-    }, [])
+// Components
+import ContentButton from "../ContentButton";
+
+export default function Image({ url }) {
+  const fetchRandomImage = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  };
+  // states
+  const [imageArray, setImage] = useState([]);
+  const [randomNumber, setNumber] = useState(0);
+
+  // effects
+  useEffect(() => {
+    fetchRandomImage().then((data) => setImage(data.message));    
+  }, []);
+
+  // handlers
+  const handleClick = () => {
+    const number = Math.floor(Math.random() * imageArray.length);
+    setNumber(number)
+  };
+
   return (
-    <div className='img-cont'>
-        <img src={image} alt="" />
+    <div className="img-cont flx column">
+      <img src={imageArray[randomNumber]} alt="" />
+      <ContentButton handleClick={handleClick} />
     </div>
-  )
+  );
 }

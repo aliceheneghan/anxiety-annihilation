4f0ce951614d2476af02aed71bf6b14747d6b118
aliceheneghan <heneghan31@gmail.com>
2022-08-18 17:28:31 +0200

    set container height and width

--- src/pages/ChoiceDog/index.jsx
+++ src/pages/ChoiceDog/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+// Components
+import Image from '../../components/Image'
+import BorderBubble from '../../components/BorderBubble'
+
 export default function ChoiceDog() {
+  const url = "https://dog.ceo/api/breeds/image/random/50"
   return (
-    <div>index</div>
+    <div className="content-cont flx cnt-cnt column">
+      <Image url={url}/>
+      <BorderBubble />
+    </div>
+  
   )
 }

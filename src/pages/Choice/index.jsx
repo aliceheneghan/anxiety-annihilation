6c16c3a7eca9f715e361eb68a0c8c5a9b72c8d9e
aliceheneghan <heneghan31@gmail.com>
2022-08-18 17:27:39 +0200

    set container height and width

--- src/pages/Choice/index.jsx
+++ src/pages/Choice/index.jsx
@@ -1,9 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-
-// Pages
-import ChoiceDog from '../ChoiceDog';
-import ChoiceSpace from '../ChoiceSpace';
-
 // Components
 import ChoiceButton from "../../components/ChoiceButton";
 import BorderBubble from "../../components/BorderBubble";
@@ -11,13 +5,8 @@ import BorderBubble from "../../components/BorderBubble";
 export default function Choice({ handleNav }) {
   return (
     <>
-      <Routes>
-        <Route path="/choice" element={<Choice handleNav={handleNav} />} />
-        <Route path="/choiceDog" element={<ChoiceDog />} />
-        <Route path="/choiceSpace" element={<ChoiceSpace />} />
-      </Routes>
       <div className="choice-cont flx cnt-cnt">
-        <ChoiceButton choice="Dog" handleNav={handleNav} />
+        <ChoiceButton choice="dog" handleNav={handleNav} />
         <ChoiceButton choice="Space" handleNav={handleNav} />
         <BorderBubble />
       </div>

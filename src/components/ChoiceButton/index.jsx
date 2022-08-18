1ff1e70a901c944a028bec6cff114248eb3f2797
aliceheneghan <heneghan31@gmail.com>
2022-08-18 11:29:38 +0200

    added slash to handleNav argument

--- src/components/ChoiceButton/index.jsx
+++ src/components/ChoiceButton/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ChoiceButton({choice, handleNav}) {
   return (
     <div className="choice-btn-cont flx cnt-cnt">
-        <button className='title-font xxl' onClick={() => handleNav(`/choice${choice}`)}>{choice}</button>
+        <button className='title-font xxl' onClick={() => handleNav(`/choice/${choice}`)}>{choice}</button>
     </div>
   )
 }

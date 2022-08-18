1b85915b8ab76ade682a0b5998c39989b977b634
aliceheneghan <heneghan31@gmail.com>
2022-08-18 10:31:06 +0200

    used choice prop inside handleNav argument

--- src/components/ChoiceButton/index.jsx
+++ src/components/ChoiceButton/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function ChoiceButton({choice}) {
+export default function ChoiceButton({choice, handleNav}) {
   return (
     <div className="choice-btn-cont flx cnt-cnt">
-        <button className='title-font xxl'>{choice}</button>
+        <button className='title-font xxl' onClick={() => handleNav(`/choice${choice}`)}>{choice}</button>
     </div>
   )
 }
